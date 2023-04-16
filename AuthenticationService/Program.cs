2046fa2cfd89ab898841506e71d3d2250025e2a9
Alexander <Aleksandr.Dedyukhin@vsegda-da.com>
2023-04-16 19:59:48 +0400

    Включил свагер для паблиш версии

--- AuthenticationService/Program.cs
+++ AuthenticationService/Program.cs
@@ -74,11 +74,11 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
 });
 
 var app = builder.Build();
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+//}
 app.UseHttpsRedirection();
 app.MapControllers();
 app.Run();
